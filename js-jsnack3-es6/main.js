698244151e6700896e6830ff7cf0b189335214c2
Ivan Mincione <ivan.mincione92@outlook.it>
2020-11-04 18:03:50 +0100

    add correct snack 3

--- js-jsnack3-es6/main.js
+++ js-jsnack3-es6/main.js
@@ -7,32 +7,45 @@ $(document).ready(function() {
 const city = [
     {
         'name': 'Roma',
+        'people': 5000000
     },
     {
         'name': 'Milano',
+        'people': 4000000
+
     },
     {
         'name': 'Firenze',
+        'people': 1000000
+
     },
     {
         'name': 'Napoli',
+        'people': 2000000
+
     },
 ];
 console.log(city);
 
 // creo una copia dell'array di partenza con SPREAD
 
-const copyCity = [...city];
-console.log(copyCity);
+// const copyCity = [...city];
+// console.log(copyCity);
+//
+// //aggiungo la nuova proprietà POSITION
+// copyCity.map((element, i) => {
+//     element.position = generateRandomLetter();
+//
+// });
+//
 
-//aggiungo la nuova proprietà POSITION
-copyCity.map((element, i) => {
-    element.position = generateRandomLetter();
+// --------   versione corretta------------
+const copyCity = city.map((element) => {
 
+    const copyElement = {...element, position:generateRandomLetter() };
+    return copyElement;
 });
-
-
-
+console.log(copyCity);
 
 
 // funzione per generare una lettera random
